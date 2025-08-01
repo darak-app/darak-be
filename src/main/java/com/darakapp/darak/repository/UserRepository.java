6613d29ee66cf58aea993d6ba99beccfeacabbc3
mango <mangoo3431@gmail.com>
2025-08-01 15:23:32 +0900

    최초 커밋

---
+++ src/main/java/com/darakapp/darak/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.darakapp.darak.repository;
+
+import com.darakapp.darak.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 필요하면 사용자 정의 메서드 추가 가능
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/darakapp/darak/service/AuthService.java
@@ -0,0 +1,23 @@
+package com.darakapp.darak.service;
+
+import com.darakapp.darak.domain.User;
+import com.darakapp.darak.exception.api.common.NotFoundApiException;
+import com.darakapp.darak.repository.UserRepository;
+import com.darakapp.darak.util.JwtUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    public AuthService(UserRepository userRepository, JwtUtil jwtUtil){
+        this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+    }
+    public String login(String username, String password){
+        User user = userRepository.findByUsername(username);
+        if(user == null)throw new NotFoundApiException();
+
+        return jwtUtil.generateToken(username);
+    }
+}
