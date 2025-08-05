22e9eabcfe16a57389e0fd4bb8de4e82eeab4547
tmin002 <ssh9930@gmail.com>
2025-08-06 04:08:54 +0900

    exception 업데이트

--- src/main/java/com/darakapp/darak/exception/api/ApiException.java
+++ src/main/java/com/darakapp/darak/exception/api/ApiException.java
@@ -2,13 +2,15 @@ package com.darakapp.darak.exception.api;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class ApiException extends RuntimeException {
-    private final String errorCode;
-    private final int httpStatusCode;
-    private final String errorMessage;
+    private String errorCode;
+    private int httpStatusCode;
+    private String errorMessage;
 
     public ApiException(ApiExceptionDetails details) {
         this.errorCode = details.getErrorCode();
--- src/main/java/com/darakapp/darak/exception/api/ApiExceptionDetails.java
+++ src/main/java/com/darakapp/darak/exception/api/ApiExceptionDetails.java
@@ -7,18 +7,29 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ApiExceptionDetails {
 
-    UNHANDLED(500, "INTERNAL000", "내부 오류가 발생했습니다. 잠시 후 다시 시도해 주세요."),
-    BAD_REQUEST(400, "BADREQUEST000", "잘못된 요청입니다."),
-    ACCESS_DENIED(403, "ACCESS000", "권한이 부족합니다."),
-    NOT_FOUND(404, "NOTFOUND000", "잘못된 경로입니다."),
+    UNHANDLED(500, "global.internal_error", "서버 내부 오류입니다. 잠시 후 다시 시도해 주세요."),
+    BAD_REQUEST(400, "global.bad_request", "잘못된 요청입니다."),
+    ACCESS_DENIED(403, "global.unauthorized", "권한이 부족합니다."),
+    NOT_FOUND(404, "global.not_found", "URL이 존재하지 않습니다."),
 
     // Sign Up
-    SIGNUP_VALIDATION_ID_EXISTS(409, "SIGNUP_VAL:001", "이미 존재하는 센터 ID 입니다."),
+    SIGNUP_VALIDATION_ID_VERIFICATION_FAILURE(
+            409,
+            "signup.validation.id_verification_failure",
+            "ID 중복확인이 완료되지 않았습니다."
+    ),
+    SIGNUP_VALIDATION_PHONE_NUMBER_VERIFICATION_FAILURE(
+            409,
+            "signup.validation.id_verification_failure",
+            "ID 중복확인이 완료되지 않았습니다."
+    ),
+    SIGNUP_VALIDATION_TOKEN_VERIFICATION_FAILURE(
+            409,
+            "signup.validation.registration_token_verification_failure",
+            "올바르지 않은 인증번호 입니다."
+    );
 
     // Sign In
-    SIGNIN_ELSE(403, "SIGNIN:000", "인증 과정 중 오류가 발생했습니다. 잠시후 다시 시도해 주세요."),
-    SIGNIN_AUTHENTICATION_FAIL(401, "SIGNIN:001", "아이디 / 비밀번호를 확인하세요."),
-    SIGNIN_MAXIMUM_SESSION_COUNT(403, "SIGNIN:002", "동시에 접속할 수 있는 최대 한도를 초과했습니다.");
 
     private final int httpStatusCode;
     private final String errorCode;
